fd1be01780ea045b4f96ff633e718707b96b7f29
krisxl007 <krisxl1987@gmail.com>
2017-03-16 18:52:25 +1100

    test git commit and push to remote

--- src/main/java/test/MyTest.java
+++ src/main/java/test/MyTest.java
@@ -2,6 +2,6 @@ package test;
 
 public class MyTest {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world! test");
     }
 }
